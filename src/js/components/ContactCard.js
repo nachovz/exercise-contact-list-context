e921e6781dad49cfa67c0c3bfc8b5e794a99e760
Ignacio Cordoba <icordoba@4geeks.co>
2018-12-08 18:47:12 +0000

    Minor fix

--- src/js/components/ContactCard.js
+++ src/js/components/ContactCard.js
@@ -15,7 +15,7 @@ class ContactCard extends React.Component{
 			<li className="list-group-item">
 				<div className="row w-100">
 					<div className="col-12 col-sm-6 col-md-3 px-0">
-						<img src={this.props.profile} alt="Mike Anamendolla" className="rounded-circle mx-auto d-block img-fluid" />
+						<img src={this.props.profile} alt={this.props.full_name} className="rounded-circle mx-auto d-block img-fluid" />
 					</div>
 					<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 						<div className="float-right">
