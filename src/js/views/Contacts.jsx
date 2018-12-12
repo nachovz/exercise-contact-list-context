4b311227987f93f0dd47fcc0ddd4afdde4087f20
Ignacio Cordoba <icordoba@4geeks.co>
2018-12-12 17:52:10 +0000

    Merging

--- src/js/views/Contacts.jsx
+++ src/js/views/Contacts.jsx
@@ -14,24 +14,33 @@ export default class Contacts extends React.Component {
 		};
 	}
 
-	render() {
-	return (
-		<div className="container">
-			<div>
-				<p className="text-right my-3">
-					<Link className="btn btn-success" to="/add">Add new contact</Link>
-				</p>
-				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
-					<ul className="list-group pull-down" id="contact-list">
-						<ContactCard onDelete={() => this.setState({ showModal: true})} />
-						<ContactCard />
-						<ContactCard />
-						<ContactCard />
-					</ul>
-				</div>
-			</div>
-			<Modal show={this.state.showModal} onClose={() => this.setState({showModal: false})} />
-		</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <p className="text-right my-3">
+            <Link className="btn btn-success" to="/add">Add new contact</Link>
+          </p>
+          <div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
+            <ul className="list-group pull-down" id="contact-list">
+              <Context.Consumer>
+                {({ store, actions}) => (
+                  store.contacts.map( (contact, index) => (
+                    <ContactCard 
+                      full_name={contact.full_name}
+                      address={contact.address}
+                      phone={contact.phone}
+                      email={contact.email}
+                      profile={contact.profile}
+                      onDelete={() => this.setState({ showModal: true})} />
+                  ))
+                )}
+              </Context.Consumer>
+            </ul>
+          </div>
+        </div>
+        <Modal show={this.state.showModal} onClose={() => this.setState({showModal: false})} />
+      </div>
+    );
+  }
 }
